5b0c8e0488b6c1270e0c7e6e4f15c05f0f992948
thirdmadman <thirdmadman@gmail.com>
2024-06-22 15:52:05 +0200

    feat: add bin log

--- log.js
+++ log.js
@@ -1,8 +1,17 @@
 #!/usr/bin/env node
+import _yargs from 'yargs';
+import {hideBin} from 'yargs/helpers';
+
+const yargs = _yargs(hideBin(process.argv));
+
+const argv = await yargs.option('height', {type: 'string', require: true}).option('width', {type: 'string', require: true}).argv;
+
+const height = argv.height;
+const width = argv.width;
 
 const main = async () => {
   console.log('Main start');
-  
+  console.log(height, width);
   console.log('Main end');
 };
 
